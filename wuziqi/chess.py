098ef0c2301e5e8044d26934f42335071004a57c
arnold <arnold@linux.com>
2018-04-01 17:59:28 +0800

    基本界面写好了

--- wuziqi/chess.py
+++ wuziqi/chess.py
@@ -4,6 +4,8 @@ from settings import Settings
 class Chess():
 	
 	def __init__(self,screen):
+		#flag 0 为黑棋
+		self.flag = 0
 		self.screen = screen
 		self.set = Settings()
 		#加载棋子图片
@@ -12,6 +14,8 @@ class Chess():
 
 		self.white_rect = self.white.get_rect()
 		self.black_rect = self.black.get_rect()
+		#棋盘坐标列表
+		self.chesslist = [[0 for i in range(self.set.columns_num + 1)]for i in range(self.set.row_num + 1)]
 
 	def chessboard(self,screen):
 		self.zerox = self.set.screen_width/2 - self.set.columns_num/2*self.set.row_width
@@ -27,25 +31,159 @@ class Chess():
 
 	def blitme(self,flag):
 		pass
-	def num_chess(self):
-		#棋盘坐标数字化，变成类似坐标1,1	1,2	....
-		pass
-	def coordinate(self,flag):
+
+	def save_chess(self):
+		if self.newx < 0 or self.newy < 0 or self.newy > self.set.row_num or self.newx > self.set.columns_num or self.chesslist[self.newy][self.newx] != 0:
+			return 0
+		if self.flag:
+			#bai qi  -1
+			self.chesslist[self.newy][self.newx] = -1
+		else:		
+			self.chesslist[self.newy][self.newx] = 1
+		#print(self.chesslist)
+
+	def coordinate(self):
 		#棋子坐标由鼠标决定
 		x,y = pygame.mouse.get_pos()
 		#鼠标点击地方的棋子坐标
 		self.newx = int((x + 25 - self.zerox)//50)
 		self.newy = int((y + 25 - self.zeroy)//50)
 		
-		print(self.newx,self.newy)
-		if self.newx < 0 or self.newy < 0 or self.newy > self.set.row_num or self.newx > self.set.columns_num:
+		#print(self.newx,self.newy)
+		if self.newx < 0 or self.newy < 0 or self.newy > self.set.row_num or self.newx > self.set.columns_num or self.chesslist[self.newy][self.newx] != 0:
 			return 0
 		self.white_rect.centerx = self.newx * 50 + self.zerox 
 		self.white_rect.bottom = self.newy * 50 + self.zeroy + 25
 		self.black_rect.centerx = self.newx * 50 + self.zerox
 		self.black_rect.bottom = self.newy * 50 + self.zeroy + 25
-		if flag:
+		if self.flag:
 			#flag 1 为白色
 			self.screen.blit(self.white,self.white_rect)		
 		else:
 			self.screen.blit(self.black,self.black_rect)		
+		self.flag = ~self.flag
+		return 1
+	
+	def win(self):
+		#白棋赢了-1 没人获胜　0
+		winflag = 0
+		i = self.chesslist[self.newy][self.newx]
+		numi = 1
+		nums = 1
+		num1 = 1
+		num2 = 1
+		#下面的以后再改，反正也是复制的
+		x = self.newx
+		y = self.newy
+		#竖直
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			y = y -1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				numi = numi + 1	
+			else:
+				break
+		x = self.newx
+		y = self.newy
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			y = y + 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				numi = numi + 1	
+			else:
+				break
+		x = self.newx
+		y = self.newy
+		#横向先左后右
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x -1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				nums = nums + 1	
+			else:
+				break
+		x = self.newx
+		y = self.newy
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x + 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				nums = nums + 1	
+			else:
+				break
+
+
+		x = self.newx
+		y = self.newy
+		#左斜
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x - 1
+			y = y - 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				num1 = num1 + 1	
+			else:
+				break
+		x = self.newx
+		y = self.newy
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x + 1
+			y = y + 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				num1 = num1 + 1	
+			else:
+				break
+
+
+		x = self.newx
+		y = self.newy
+		#右斜
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x + 1
+			y = y - 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				num2 = num2 + 1	
+			else:
+				break
+		x = self.newx
+		y = self.newy
+		while(x >= 0 and y >= 0 and x <= self.set.columns_num and y <= self.set.row_num):
+			a = self.chesslist[y][x]
+			x = x - 1
+			y = y + 1
+			if x < 0 or y < 0 or x > self.set.columns_num or y > self.set.row_num:
+				break
+			b = self.chesslist[y][x]
+			if a == b:
+				num2 = num2 + 1	
+			else:
+				break
+
+
+		if numi > 4 or nums > 4 or num1 > 4 or num2 > 4:
+			winflag = i
+			return winflag
+		return 0
--- wuziqi/wuziqi.py
+++ wuziqi/wuziqi.py
@@ -4,22 +4,6 @@ import sys
 from pygame.locals import *
 from settings import Settings
 from chess import Chess
-flag = 0
-'''
-#传入屏幕对象
-def init_chessboard(screen):
-	qiset = Settings()
-	zerox = qiset.screen_width/2 - qiset.columns_num/2*qiset.row_width
-	zeroy = qiset.screen_height/2 - qiset.row_num/2*qiset.row_width
-	
-	#画棋盘
-	for i in range(0,qiset.row_num + 1):
-		a = i * qiset.row_width
-		pygame.draw.line(screen,qiset.qipan_color,(zerox,zeroy + a),(zerox + qiset.row_width*qiset.columns_num,zeroy + a),5)
-	for i in range(0,qiset.columns_num + 1):
-		a = i * qiset.row_width
-		pygame.draw.line(screen,qiset.qipan_color,(zerox + a,zeroy),(zerox + a,zeroy + qiset.row_num * qiset.row_width),5)
-'''
 def run_game():
 	global flag
 	
@@ -29,13 +13,12 @@ def run_game():
 	screen = pygame.display.set_mode((qiset.screen_width,qiset.screen_height))
 	pygame.display.set_caption("五子棋")
 	chess = Chess(screen) 
-	
+	a = 0
 		
 	#bg_color
 	screen.fill(qiset.bg_color)
 	#绘制棋盘
 	chess.chessboard(screen)	
-
 	#main()
 	while True:
 		for event in pygame.event.get():
@@ -48,16 +31,20 @@ def run_game():
 						if i == 0:
 							#left button
 							#chess.blitme(flag)
-							chess.coordinate(flag)
-							flag = ~flag
+							a=chess.coordinate()
+							if a:
+								chess.save_chess()
+								b = chess.win()
+								if b == 1:
+									print('白棋赢了')
+								elif b == -1:
+									print('黑棋赢了')
 						elif i == 1:
 							#mouse wheel
 							pass
 						elif i == 2:
 							pass
 							#right button
-							#chess.coordinate(flag)
-
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
 run_game()
